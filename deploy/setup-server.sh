57e2d570b9146da751ed197c152bc20ffeb1b699
Andrew Herd <aherd2985@gmail.com>
2026-04-02 20:22:09 -0500

    Fix missing color

--- deploy/setup-server.sh
+++ deploy/setup-server.sh
@@ -18,6 +18,7 @@ DOMAIN="${DOMAIN:?Set DOMAIN environment variable, e.g.: sudo DOMAIN=msg.example
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
+CYAN='\033[0;36m'
 NC='\033[0m' # No Color
 
 log() { echo -e "${GREEN}[+]${NC} $1"; }
